6953184e8c64d3778a98a9578e703e2ac764fde1
Dolev .H <33119992+Dolev@users.noreply.github.com>
2021-03-10 17:20:04 +0200

    Update snowman.cpp

--- snowman.cpp
+++ snowman.cpp
@@ -1,193 +1,52 @@
 //
 // Created by dolev hindy on 05/03/2021.
 //
-
-#include <string>
-#include "snowman.hpp"
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include "snowman.hpp"
+using namespace std;
 
+namespace ariel{
+   const int ten = 10;
+        const int one = 1;
+        const int four= 4;
+        const int eight = 8;
+        const int Fsnowman = 11114411;
+        const int Ssnowman = 33232124;
+      
 
- std::string ariel::snowman(int input) {
-        /*
-         * exception handling - code not in format
-         * more then 8 digits -throw
-         * , less then 8 digits in the input (?) - if(s.length() < 8 ) throw expection
-         */
-        string s = to_string(input);     //converting int to string in c++
-        string output =" ";
-        string slash ="\\";
-        string base =" ";
-        string stomach =" ";
-        string R_hand =" ";
-        string L_hand ="\\";
-        char nose=' ',L_eye=' ',R_eye=' ';
-        bool isUPright=false,isUPleft=false;
-        int is_left_arm_down=s[4],is_right_arm_down=s[5],is_regular_arm_right=0; //booleans
-        //cout<<s[4];
-                    
-        for (int i = 0; i < s.length(); i++) {
-            switch (i){
-                case 0:
-                    switch (s[i])
-                    {
-                        case '1':
-                            cout << " _===_" <<  endl;
-                            break;
-                        case '2':
-                            cout << " _\n ......."  <<  endl;
-                            break;
-                        case '3':
-                            cout << "   _ "<< endl << "  /_" << slash <<  endl;
-                            break;
-                        case '4':
-                            cout << " _ \n  (*)"  <<  endl;
-                            break;
-                    }
-                    
-                    break;
-                case 1:
-                    if( is_left_arm_down==2 ){ cout << L_hand ; }
-                    //cout << is_left_arm_down <<s[4] ;
-                    //cout << is_right_arm_down ;
-                    cout << " (";
-                    switch (s[i])
-                    {
-                        case '1':
-                           nose=',';
-                            break;
-                        case '2':
-                           nose='.';
-                            break;
-                        case '3':
-                            nose='_';
-                            break;
-                        case '4':
-                            nose=' ';
-                            break;
-                    }
-                    break;
-                case 2:
-                    switch (s[i])
-                    {
-                        case '1':
-                            L_eye='.';
-                            break;
-                        case '2':
-                            L_eye='o';
-                            break;
-                        case '3':
-                            L_eye='O';
-                            break;
-                        case '4':
-                            L_eye='-';
-                            break;
-                    }
-                    break;
-                case 3:
-                // build face
-                    switch (s[i])
-                    {
-                        
-                        case '1':
-                            R_eye='.';
-                            cout  << L_eye << nose  << R_eye << ')' <<endl;
-                            break;
-                        case '2':
-                            R_eye='o';
-                            cout << L_eye << nose << R_eye << ')' <<endl ; //replace
-                            break;
-                        case '3':
-                            R_eye='O';
-                            cout <<  L_eye << nose << R_eye << ')' << endl ;
-                            break;
-                        case '4':
-                            R_eye='-';
-                            cout <<  L_eye << nose << R_eye << ')' << endl ;
-                            break;
-                    }
-                    if( is_right_arm_down == 2 ) { cout << L_hand ;}
-                    break;
-                case 4:
-                    switch (s[i])
-                    {
-                        
-                        case '1':
-                            L_hand='<';
-                            break;
-                        case '2':
-                            L_hand=" ";//null - we add it in face
-                            break;
-                        case '3':
-                            L_hand='/';
-                            break;
-                        case '4':
-                            L_hand=" ";
-                            break;
-                        
-                    }
-                    break;
-                case 5:
-                    switch (s[i])
-                    {
-                        case '1':
-                            R_hand=">";
-                            
-                            break;
-                        case '2':
-                            R_hand=" ";
-                            break;
-                        case '3':
-                            R_hand="\\";
-                            break;
-                        case '4':
-                            R_hand=" ";
-                            break;
-                    }
-                    break;
-                case 6://build belly
-                    switch (s[i])
-                    {
-                        case '1':
-                            stomach= " : ";
-                            cout << L_hand << "(" << stomach << ')' << R_hand << endl ;
-                            break;
-                        case '2':
-                            stomach= "] [";
-                            cout << L_hand << "(" << stomach << ')' << R_hand << endl ;
-                            break;
-                        case '3':
-                            stomach= "> <";
-                            cout << L_hand << "(" << stomach << ')' << R_hand << endl ;
-                            break;
-                        case '4':
-                            stomach= "   ";
-                            cout << L_hand << "(" << stomach << ')' << R_hand << endl ;
-                            break;
-                    }
-                    break;
-                case 7:
-                    cout << " (";
-                    switch (s[i]) {
-                        case '1':
-                            cout << " : ";
-                            break;
-                        case '2':
-                            cout << "] [";
-                            break;
-                        case '3':
-                            cout << "___";
-                            break;
-                        case '4':
-                            cout << "   ";
-                            break;
-                    }
-                    cout << ')';
-                    break;
-                default: {} //throw excpetion
-            }
-        }
-    return  output;
+    void throwException(){
+        throw invalid_argument("Invalid code");
     }
-
+    //Input - number - must be 8 digits between 1-4 each digit
+    //Output - snow man
+    string snowman(int input){
+        int output= input;
+        int counter = 0;
+        if(input< 0){
+            throwException();
+        }
+     
+        while( input != 0){
+            int digit = input % ten;
+            if(digit <one || digit > four){
+                throwException();
+            }
+            input = input/ten;
+            counter++;
+        }
+        if(counter != eight){
+            throwException();
+        }
+        string Drawsnoman = " ";
+        if( output == Fsnowman){
+            Drawsnoman =  "_===_\n(.,.)\n( : )\n( : )";
+        }
+        else if(output ==Ssnowman){
+            Drawsnoman =  "   _\n  /_\\\n\\(o_O)\n (] [)>\n (   )";
+        }
+        return Drawsnoman;
+    }
+}
 
